5fe78ee729c6713efc47bc34353da245e3e0e8c3
bkaoukine <b.koukin@gmail.com>
2024-03-30 23:11:41 +0000

    4. Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,8 @@
 """Index that for the app status."""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.engine import db_storage
 
 
 @app_views.route('/status')
@@ -9,3 +11,15 @@ def status():
     """Statu method for the app statu."""
     statu = {"status": "OK"}
     return jsonify(statu)
+
+
+@app_views.route('/stats')
+def numbofobj():
+    """NUMOFOBJ retrieves the number of each objects by type."""
+    stats = {}
+
+    for cls_name in db_storage.classes:
+        count = storage.count(cls_name)
+        stats[cls_name] = count
+
+    return jsonify(stats)
