4caa15497b231e4617de71cb2f5cd365bfb23409
bkaoukine <b.koukin@gmail.com>
2024-03-31 00:16:51 +0000

    5. Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,8 @@
 
 from api.v1.views import app_views
 from flask import Flask
+from flask import make_response
+from flask import jsonify
 from models import storage
 import os
 
@@ -16,6 +18,12 @@ app.register_blueprint(app_views)
 def teardown_storage(exception):
     """Close the storage connection at the end of the request."""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handle the error response to retruns json instead of HTML."""
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
